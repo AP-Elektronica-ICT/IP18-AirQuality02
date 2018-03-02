5ef31b2070e150d125a486ffcf06a6316cf63623
Kevin Van de Vyver <rosky100@telenet.be>
2018-03-02 12:36:38 +0100

    Testmap functionality

--- src/app/TestMap/testmap.component.ts
+++ src/app/TestMap/testmap.component.ts
@@ -7,9 +7,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestMapComponent implements OnInit {
 
-  constructor() { }
+  data: Object[];
+
+  constructor() { 
+    this.data = [
+      {
+        room: 101,
+        temperature: 25,
+        humidity: 40,
+        co2: 700,
+        sound: 85,
+        illuminantie: 80,
+        hide: true
+      },
+      {
+        room: 102,
+        temperature: 20,
+        humidity: 60,
+        co2: 500,
+        sound: 65,
+        illuminantie: 90,
+        hide: true
+      },
+      {
+        room: 103,
+        temperature: 23,
+        humidity: 30,
+        co2: 800,
+        sound: 70,
+        illuminantie: 20,
+        hide: true
+      },
+      {
+        room: 104,
+        temperature: 20,
+        humidity: 10,
+        co2: 300,
+        sound: 20,
+        illuminantie: 100,
+        hide: true
+      },
+      {
+        room: 105,
+        temperature: 19,
+        humidity: 90,
+        co2: 1000,
+        sound: 30,
+        illuminantie: 10,
+        hide: true
+      }
+    ]
+  }
 
   ngOnInit() {
+  }
+
+  showData(room: number){
+      
   }
 
 }
