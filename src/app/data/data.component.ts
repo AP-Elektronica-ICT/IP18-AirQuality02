f7753d9d35e322cb94cefde4d182f908ae003e2a
Seppe De Beule <seppe.debeule@telenet.be>
2018-04-17 15:17:30 +0200

    Removed unnecessary textbox and progress on historical data

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -5,12 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './data.component.html',
   styleUrls: ['./data.component.scss']
 })
+
 export class DataComponent implements OnInit {
   // lineChart
   public lineChartData: Array<any> = [
     { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
     { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-    { data: [18, 48, 77, 9, 100, 27, 40], label: 'Series C' }
+    { data: [18, 48, 77, 12, 100, 27, 40], label: 'Series C' }
   ];
   public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
   public lineChartOptions: any = {
@@ -55,6 +56,8 @@ export class DataComponent implements OnInit {
     }
     this.lineChartData = _lineChartData;
   }
+
+  
 
   // events
   public chartClicked(e: any): void {
